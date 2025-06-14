cc62c255574090fea885cb9a75e862f44e817dc1
Gabriela <gabriela.carniel@sellflux.com.br>
2025-06-14 14:42:26 -0300

    feat: add Footer component

---
+++ src/components/footer.tsx
@@ -0,0 +1,10 @@
+export const Footer = () => {
+  return (
+    <footer className='py-6 text-center text-zinc-400 text-xs'>
+      <p>
+        <span className='font-bold'>Gabriela Carniel</span> © 2025. Todos os
+        direitos reservados.
+      </p>
+    </footer>
+  )
+}
